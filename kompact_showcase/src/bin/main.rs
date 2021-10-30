f0f7e322d77edf87490149626f8bc101f7e84f64
devsensa <person.sensa@gmail.com>
2021-10-30 17:12:24 +0300

    refactoring: remove unused component

--- kompact_showcase/src/bin/main.rs
+++ kompact_showcase/src/bin/main.rs
@@ -23,56 +23,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut config = KompactConfig::default();
     let system = config.build().expect("system");
-    let component = system.create(HelloWorldComponent::new);
+
     let dog_fact = system.create(DogFactComponent::new);
     let dog_fact_ref = dog_fact.actor_ref().hold().expect("live");
+    system.start(&dog_fact);
+
     let answer = dog_fact_ref.ask(Ask::of(DogFactRequest));
     let handle = system.spawn(async move {
         let result = answer.await.unwrap();
         dbg!(result.0);
     });
 
-    system.start(&component);
-    system.start(&dog_fact);
     system.await_termination();
     Ok(())
 }
-
-
-#[derive(ComponentDefinition)]
-struct HelloWorldComponent {
-    ctx: ComponentContext<Self>
-}
-impl HelloWorldComponent {
-    pub fn new() -> HelloWorldComponent {
-        HelloWorldComponent {
-            ctx: ComponentContext::uninitialised()
-        }
-    }
-}
-
-enum CatFactMessage {
-    CatFactRequest,
-    CatFactResponse(String)
-}
-
-impl Actor for HelloWorldComponent {
-    type Message = ();
-
-    fn receive_local(&mut self, msg: Self::Message) -> Handled {
-        todo!()
-    }
-
-    fn receive_network(&mut self, msg: NetMessage) -> Handled {
-        Handled::Ok
-    }
-}
-
-impl ComponentLifecycle for HelloWorldComponent {
-    fn on_start(&mut self) -> Handled {
-        info!(self.ctx.log(), "Hello World!");
-        // self.ctx().system().shutdown_async();
-        Handled::Ok
-    }
-}
-
